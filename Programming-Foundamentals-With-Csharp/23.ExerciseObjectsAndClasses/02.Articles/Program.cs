3e3670a39fba285b273ace2a31bcde9113ae8336
skider96 <danailqkiq@gmail.com>
2023-11-10 20:21:41 +0200

    50% of second problem

--- Programming-Foundamentals-With-Csharp/23.ExerciseObjectsAndClasses/02.Articles/Program.cs
+++ Programming-Foundamentals-With-Csharp/23.ExerciseObjectsAndClasses/02.Articles/Program.cs
@@ -3,8 +3,48 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { 
+            Article myArticle = new Article();
+
+
         }
     }
+
+    class Article
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public Article editContent(string content)
+        {
+            return new Article { Content = Console.ReadLine() };
+        }
+
+        public Article ChangeAuthor(string author)
+        {
+            return new Article { Author = Console.ReadLine() };
+        }
+
+        public Article RenameNewTitle(string title)
+        {
+            return new Article { Title = Console.ReadLine() };
+        }
+
+        public override string ToString()
+        {
+            return "{title} - {content}: {author}";
+        }
+    }
+
 }
+
+/*
+ *Edit (new content) – change the old content with the new one
+   • ChangeAuthor (new author) – change the author
+   • Rename (new title) – change the title of the article
+   • Override the ToString method – print the article in the following format: 
+   "{title} - {content}: {author}
+ */
