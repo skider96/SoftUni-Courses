b27514f1a8b6c68d100ecf6defa8d1ce04b4052b
skider96 <danailqkiq@gmail.com>
2023-11-11 16:00:18 +0200

    problem solved

--- Programming-Foundamentals-With-Csharp/23.ExerciseObjectsAndClasses/04.Students/Program.cs
+++ Programming-Foundamentals-With-Csharp/23.ExerciseObjectsAndClasses/04.Students/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace _04.Students
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace _04.Students
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            List<Students> studentsList = new List<Students>();
+
+            for (var i = 0; i < n; i++)
+            {
+                string[] inputOfStudent = Console.ReadLine().Split();
+                string firstName = inputOfStudent[0];
+                string lastName = inputOfStudent[1];
+                float grade = float.Parse(inputOfStudent[2]);
+                studentsList.Add(new Students(firstName, lastName, grade));
+            }
+
+            studentsList = studentsList.OrderByDescending(students=> students.Grade).ToList();
+
+            foreach (var student in studentsList)
+            {
+                System.Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+            }
         }
+    }
+    class Students
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public float Grade { get; set; }
+
+        public Students(string firstName, string lastName, float grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+        
     }
 }
