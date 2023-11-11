6fc448db2181856c5d7cfe2e5c17573bfa16c24a
skider96 <danailqkiq@gmail.com>
2023-11-11 13:51:38 +0200

    10% of the problem solved

--- Programming-Foundamentals-With-Csharp/23.ExerciseObjectsAndClasses/03.Articles2.0/Program.cs
+++ Programming-Foundamentals-With-Csharp/23.ExerciseObjectsAndClasses/03.Articles2.0/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] input = Console.ReadLine()
+                .Split(", ")
+                .ToArray();
+            
+            string title = input[0];
+            string content = input[1];
+            string author = input[2];
+
+            Article myArticle = new Article(title, content, author);
+
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] secondInput = Console.ReadLine().Split(": ");
+               
+            }
+
+            Console.WriteLine(myArticle);
+        }
+
+    }
+
+    class Article
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
         }
     }
+
 }
