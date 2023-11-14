b2c36088141a6f3b4b772d438c6a8d443e9d2d1a
skider96 <danailqkiq@gmail.com>
2023-11-14 20:08:38 +0200

    80% solved

--- Programming-Foundamentals-With-Csharp/23.ExerciseObjectsAndClasses/06.VehicleCatalogue/Program.cs
+++ Programming-Foundamentals-With-Csharp/23.ExerciseObjectsAndClasses/06.VehicleCatalogue/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace _06.VehicleCatalogue
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+namespace _06.VehicleCatalogue
 {
     internal class Program
     {
@@ -7,6 +11,7 @@
             //"{typeOfVehicle} {model} {color} {horsepower}"
 
             List<Car> cars = new List<Car>();
+            List<string> listCarTypes = new List<string>();
             string command;
             while ((command = Console.ReadLine()) != "End")
             {
@@ -14,21 +19,37 @@
 
                 string typeOfVehicle = input[0];
                 string model = input[1];
+                listCarTypes.Add(typeOfVehicle);
                 string color = input[2];
                 int horsepower = int.Parse(input[3]);
                 Car currentCar = new Car(typeOfVehicle, model, color, horsepower);
                 cars.Add(currentCar);
             }
 
-
             while ((command = Console.ReadLine()) != "Close the Catalogue")
             {
+                Car car = new Car(command);
 
-                if (cars.Any(c => c.TypeOfVehicle == command))
+                for (int i = 0; i < cars.Count; i++)
                 {
-                    System.Console.WriteLine(string.Join("", cars.Where(c => c.TypeOfVehicle == command)));
+                    if (cars[i].Model == command)
+                    {
+                        Console.WriteLine(cars[i]);
+                    }
                 }
             }
+
+            int cars = cars.Where(c => c.TypeOfVehicle == "car");
+            
+
+
+
+            /*
+            format: "{typeOfVehicles} have average horsepower of {averageHorsepower}."
+               The average horsepower is calculated by dividing
+            the sum of the horsepower of all vehicles of the given type by
+               the total count of all vehicles from that type.
+            Format the answer to the second digit after the decimal point.*/
         }
     }
 
@@ -41,14 +62,28 @@
 
         public Car(string argType, string argModel, string argColor, int argHP)
         {
+            var firstLetter = FirstLetterCapitalize(argType);
+            argType = firstLetter + argType.Substring(1);
             TypeOfVehicle = argType;
             Model = argModel;
             Color = argColor;
             Horsepower = argHP;
         }
+
+        private static char FirstLetterCapitalize(string argType)
+        {
+            char[] charsArgType = argType.ToCharArray();
+            char firstLetter = char.ToUpper(charsArgType[0]);
+            return firstLetter;
+        }
+
+        public Car(string argModel)
+        {
+            Model = argModel;
+        }
         public override string ToString()
         {
-            return $"{TypeOfVehicle} {Model} {Color} {Horsepower}";
+            return $"Type: {TypeOfVehicle}\nModel: {Model}\nColor: {Color}\nHorsepower: {Horsepower}";
         }
     }
 }
@@ -59,4 +94,8 @@ car Ford green 120
 car Ferrari red 550
 car Lamborghini orange 570
 End
+Ferrari
+Ford
+Man
+Close the Catalogue
 */
---
+++ Programming-Foundamentals-With-Csharp/23.ExerciseObjectsAndClasses/06.VehicleCatalogue/tempCodeRunnerFile.cs
@@ -0,0 +1,8 @@
+ // while ((command = Console.ReadLine()) != "Close the Catalogue")
+            // {
+
+            //     if (cars.Any(c => c.TypeOfVehicle == command))
+            //     {
+            //         System.Console.WriteLine(string.Join("", cars.Where(c => c.TypeOfVehicle == command)));
+            //     }
+            // }
