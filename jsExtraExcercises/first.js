85f73196cff5ffe2d14362b19f62d25b840d1663
skider96 <danailqkiq@gmail.com>
2023-11-15 16:30:10 +0200

    javascript and csharp tests

--- jsExtraExcercises/first.js
+++
@@ -1,5 +0,0 @@
-console.log("Error creating");
-
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-}
---
+++ jsExtraExcercises/js-basics/tests/first.js
@@ -0,0 +1,9 @@
+console.log("Error creating");
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+// }
+
+
+let name = 'Danail';
+console.log(typeof name);
