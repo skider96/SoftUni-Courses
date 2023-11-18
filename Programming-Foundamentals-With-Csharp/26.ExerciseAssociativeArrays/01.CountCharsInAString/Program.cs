c5a7778666e82f3b9bcc75200bb7a7b39061cba9
skider96 <danailqkiq@gmail.com>
2023-11-18 14:39:51 +0200

    solved 2 problems in c#

---
+++ Programming-Foundamentals-With-Csharp/26.ExerciseAssociativeArrays/01.CountCharsInAString/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+text
+-------
+text text text
+ */
+namespace _01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+         
+            Dictionary<char, int> keyValuePairs = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (keyValuePairs.ContainsKey(input[i])) keyValuePairs[input[i]]++;
+                else keyValuePairs.Add(input[i], 1);
+            }
+            keyValuePairs.Remove(' ');
+            foreach (var key in keyValuePairs)
+            {
+                Console.WriteLine($"{key.Key} -> {key.Value}");
+            }
+        }
+    }
+}
---
+++ Programming-Foundamentals-With-Csharp/26.ExerciseAssociativeArrays/02.AMinerTask/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _02.AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resourse = new Dictionary<string, int>();
+            string command;
+            int i = 0;
+            string previousCommand = "";
+            while ((command = Console.ReadLine()) != "stop")
+            {
+                if (i % 2 == 0)
+                {
+                    if (resourse.ContainsKey(command))
+                    {
+                        previousCommand = command; 
+                        i++;
+                        continue;
+                    }
+                    else resourse.Add(command, 0);
+                }
+                else
+                {
+                    if (resourse.ContainsKey(previousCommand))
+                    {
+                        resourse[previousCommand] += int.Parse(command);
+                    }
+                  
+                }
+                previousCommand = command;
+                i++;
+            }
+
+            foreach (var kvp in resourse)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
+/*
+Gold
+155
+Silver
+10
+Copper
+17
+stop
+ */
