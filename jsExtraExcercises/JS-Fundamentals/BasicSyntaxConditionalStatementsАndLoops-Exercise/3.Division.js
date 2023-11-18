bffff5056c731d0d6c9798d90158ce40c19f7bb6
skider96 <danailqkiq@gmail.com>
2023-11-18 11:29:55 +0200

    solved 3.Division.js

---
+++ jsExtraExcercises/JS-Fundamentals/BasicSyntaxConditionalStatementsАndLoops-Exercise/3.Division.js
@@ -0,0 +1,16 @@
+function Division (number) {
+  if (number % 2 === 0 && number % 3 === 0 && number % 6 === 0) {
+    if (number % 10 === 0) {
+      console.log(`The number is divisible by 10`)
+    } else console.log(`The number is divisible by 6`)
+  } else if (number % 2 === 0 && number % 10 === 0)
+    console.log(`The number is divisible by 10`)
+  else if (number % 2 === 0 && number % 3 === 0)
+    console.log(`The number is divisible by 3`)
+  else if (number % 7 === 0) console.log(`The number is divisible by 7`)
+  else if (number % 10 === 0) console.log(`The number is divisible by 10`)
+  else if (number % 3 === 0) console.log(`The number is divisible by 3`)
+  else if (number % 2 === 0) console.log(`The number is divisible by 2`)
+  else console.log('Not divisible')
+}
+//Division(15)
