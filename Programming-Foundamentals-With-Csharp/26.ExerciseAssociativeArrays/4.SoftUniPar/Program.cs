03581a9b0ce137b59538a01bda0c16fa5b48f291
skider96 <danailqkiq@gmail.com>
2023-11-19 15:26:32 +0200

    solving

---
+++ Programming-Foundamentals-With-Csharp/26.ExerciseAssociativeArrays/4.SoftUniPar/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _4.SoftUniPar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int usersCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < usersCount; i++)
+            {
+                // "register {username} {licensePlateNumber}
+
+                string[] arguments = Console.ReadLine().Split();
+
+                string command = arguments[0];
+                string username = arguments[1];
+                if (arguments.Length > 2)
+                {
+                    string licensePlate = arguments[2];
+                }
+
+                string something = 'hui';
+            }
+        }
+    }
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string LicensePlate { get; set; }
+
+        public User(string userName, string licensePlate)
+        {
+            UserName = userName;
+            LicensePlate = licensePlate;
+        }
+
+    }
+}
