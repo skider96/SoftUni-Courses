3ecd76b4df5f6e98163658a970c0d16c04904fd4
skider96 <danailqkiq@gmail.com>
2023-11-22 19:46:12 +0200

    solved SoftUniParking

--- Programming-Foundamentals-With-Csharp/26.ExerciseAssociativeArrays/4.SoftUniPar/Program.cs
+++ Programming-Foundamentals-With-Csharp/26.ExerciseAssociativeArrays/4.SoftUniPar/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             int usersCount = int.Parse(Console.ReadLine());
+            Dictionary<string, User> users = new Dictionary<string, User>();
 
             for (int i = 0; i < usersCount; i++)
             {
@@ -14,10 +15,46 @@
 
                 string command = arguments[0];
                 string username = arguments[1];
+                string licensePlate = "";
                 if (arguments.Length > 2)
                 {
-                    string licensePlate = arguments[2];
+                    licensePlate = arguments[2];
                 }
+
+                // 
+                // "{username} registered {licensePlateNumber} successfully"
+                User userInfo = new User(username, licensePlate);
+
+                if (command == "register")
+                {
+                    if (!users.ContainsKey(username))
+                    {
+
+                        users.Add(username, userInfo);
+                        Console.WriteLine($"{username} registered {licensePlate} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlate}");
+                    }
+                }
+                else if (command == "unregister")
+                {
+                    if (!users.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        users.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                }
+            }
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.Key} => {user.Value.LicensePlate}");
             }
         }
     }
@@ -32,5 +69,16 @@
             UserName = userName;
             LicensePlate = licensePlate;
         }
+
     }
 }
+
+/*
+ *
+5
+register John CS1234JS
+register George JAVA123S
+register Andy AB4142CD
+register Jesica VR1223EE
+unregister Andy
+ */
