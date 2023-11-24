7ec7fc55996895cd42489913f19f2704c871e1bf
skider96 <danailqkiq@gmail.com>
2023-11-24 19:31:45 +0200

    solved first exercise

---
+++ Programming-Foundamentals-With-Csharp/29.Exercise-TextProcessing/1.ValidUsernames/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _1.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ").ToArray();
+
+            foreach (var username in input)
+            {
+                if (username.Length is >= 3 and <= 16)
+                {
+
+                    if (username.All(c => char.IsLetterOrDigit(c) || c == '-' || c == '_'))
+                    {
+                        Console.WriteLine(username);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Programming-Foundamentals-With-Csharp/29.Exercise-TextProcessing/2CharacterMultiplier/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
