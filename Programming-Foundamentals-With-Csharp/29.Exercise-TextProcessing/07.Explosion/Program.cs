c9602fd2d5296094f2616df3958315e619d4bf48
skider96 <danailqkiq@gmail.com>
2023-11-26 23:03:41 +0200

    10% solved

---
+++ Programming-Foundamentals-With-Csharp/29.Exercise-TextProcessing/07.Explosion/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Explosions are marked with '>'.Immediately after the mark, there will be an integer, which signifies the strength
+            //of the explosion.
+            //You should remove x characters(where x is the strength of the explosion), starting after the punched character
+            //    ('>').
+            //If you find another explosion mark('>') while you're deleting characters, you should add the strength to your
+            //previous explosion.
+            //When all characters are processed, print the string without the deleted characters.
+            //You should not delete the explosion character – '>', but you should delete the integers, which represent the
+            //strength.
+
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input.Length -)
+                {
+                    
+                }
+            }
+        }
+    }
+}
