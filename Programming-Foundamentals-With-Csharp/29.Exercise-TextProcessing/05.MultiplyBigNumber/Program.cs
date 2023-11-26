bc300aebb2b836ebd55594880ee9634a6d2136d2
skider96 <danailqkiq@gmail.com>
2023-11-26 22:08:48 +0200

    solved 2 problems

---
+++ Programming-Foundamentals-With-Csharp/29.Exercise-TextProcessing/05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+namespace _05.MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger firstNumber = BigInteger.Parse(Console.ReadLine());
+            BigInteger secondNumber = BigInteger.Parse(Console.ReadLine());
+
+            BigInteger result = firstNumber * secondNumber;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Programming-Foundamentals-With-Csharp/29.Exercise-TextProcessing/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> input = Console.ReadLine().ToList();
+            List<char> output = new List<char>();
+            char previousChar = ' ';
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (i == 0)
+                {
+                    output.Add(input[i]);
+                }
+                else if (!(output[output.Count - 1] == input[i]))
+                {
+                    output.Add(input[i]);
+                }
+            }
+
+            foreach (var element in output)
+            {
+                Console.Write(element);
+            }
+        }
+    }
+}
