a9b4dfbc7a2be07e164fde9c88063da79fee7f6f
skider96 <danailqkiq@gmail.com>
2023-11-26 19:26:02 +0200

    solved 03.ExtractFile

---
+++ Programming-Foundamentals-With-Csharp/29.Exercise-TextProcessing/3.ExtractFile/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _3.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split('\\', '.');
+
+            string fileName = input[input.Length - 2];
+            string extensionType = input[input.Length - 1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extensionType}");
+        }
+    }
+}
+/*
+ C:\Internal\training-internal\Template.pptx
+ */
