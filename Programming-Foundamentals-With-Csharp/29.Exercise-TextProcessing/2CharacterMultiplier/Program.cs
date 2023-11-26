e2d6666ebd956d1f04541f03d788ceea4c3f67d1
skider96 <danailqkiq@gmail.com>
2023-11-26 11:25:49 +0200

    solved 02.CharacterMultiplie

--- Programming-Foundamentals-With-Csharp/29.Exercise-TextProcessing/2CharacterMultiplier/Program.cs
+++ Programming-Foundamentals-With-Csharp/29.Exercise-TextProcessing/2CharacterMultiplier/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace _2CharacterMultiplier
+﻿namespace _02.CharacterMultiplier
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string[] inputStrings = Console.ReadLine().Split();
+
+            char[] firstStringChars = inputStrings[0].ToCharArray();
+            char[] secondStringChars = inputStrings[1].ToCharArray();
+
+            var sum = SumChars(inputStrings, secondStringChars, firstStringChars);
+
+            Console.WriteLine(sum);
+        }
+
+        private static int SumChars(string[] inputStrings, char[] secondStringChars, char[] firstStringChars)
+        {
+            int n = inputStrings[0].Length >= inputStrings[1].Length ? inputStrings[0].Length : inputStrings[1].Length;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i >= inputStrings[0].Length)
+                {
+                    sum += secondStringChars[i];
+                }
+                else if (i >= inputStrings[1].Length)
+                {
+                    sum += firstStringChars[i];
+                }
+                else sum += firstStringChars[i] * secondStringChars[i];
+            }
+
+            return sum;
         }
     }
 }
