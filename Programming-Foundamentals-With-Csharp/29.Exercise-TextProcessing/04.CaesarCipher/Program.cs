431e182d0ebd990a4a7ee7ae93b76259376e55f3
skider96 <danailqkiq@gmail.com>
2023-11-26 20:00:21 +0200

    solved 04.CaesarCipher

---
+++ Programming-Foundamentals-With-Csharp/29.Exercise-TextProcessing/04.CaesarCipher/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _04.CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+         char[] encryptedChars = input.ToCharArray();
+        
+            for (int i = 0; i < encryptedChars.Length; i++)
+            {
+                int asciiEquivalent = (int)encryptedChars[i] + 3;
+                encryptedChars[i] = (char)asciiEquivalent;
+            }
+
+            foreach (var encryptedChar in encryptedChars)
+            {
+                Console.Write(encryptedChar);
+            }
+        }
+    }
+}
