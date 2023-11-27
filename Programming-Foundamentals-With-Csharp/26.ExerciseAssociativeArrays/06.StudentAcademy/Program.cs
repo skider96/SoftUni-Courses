bac09c8f19fbbaf2b0119280a4c027f7e0cb5891
skider96 <danailqkiq@gmail.com>
2023-11-27 18:50:08 +0200

    10% solved

---
+++ Programming-Foundamentals-With-Csharp/26.ExerciseAssociativeArrays/06.StudentAcademy/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _06.StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a program that keeps the information about students and their grades.
+You will receive n pair of rows. First, you will receive the student's name, after that, you will receive their grade.
+Check if the student already exists and if not, add him. Keep track of all grades for each student.
+When you finish reading the data, keep the students with an average grade higher than or equal to 4.50.
+Print the students and their average grade in the following format:
+"{name} –> {averageGrade}"
+Format the average grade to the 2
+nd decimal place.*/
+
+            int n = int.Parse(Console.ReadLine());
+
+        }
+    }
+
+    class Students
+    {
+        public Students(string studentName, double studentGrades)
+        {
+            StudentName = studentName;
+            StudentGrades = studentGrades;
+        }
+
+        public string StudentName { get; set; }
+
+        public double StudentGrades { get; set; }
+
+
+    }
+}
