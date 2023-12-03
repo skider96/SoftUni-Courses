87464808af19b1847edbf2c745377db6fdb9fcc4
skider96 <danailqkiq@gmail.com>
2023-12-03 10:02:38 +0200

    working

--- Programming-Foundamentals-With-Csharp/38.RegularFinalExam/03.Zadacha/Program.cs
+++ Programming-Foundamentals-With-Csharp/38.RegularFinalExam/03.Zadacha/Program.cs
@@ -4,8 +4,14 @@
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             
 =======
+            string name = "George";
+            name[2] = "m";
+            System.Console.WriteLine(name[2]);
+
+>>>>>>> Stashed changes
         }
     }
 }
