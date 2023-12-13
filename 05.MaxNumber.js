dce4908b9da789d30174184e2305943b6ac29f7b
skider96 <138997737+skider96@users.noreply.github.com>
2023-12-13 19:59:46 +0200

    added new excercise

---
+++ 05.MaxNumber.js
@@ -0,0 +1,7 @@
+//Write a function to find all the top integers 
+//in an array. A top integer is an integer, 
+//which is bigger than all the elements to its right. 
+//Output
+//Print all top integers on the console,
+// separated by a single space.
+
