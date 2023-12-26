232a15b4210110c63ba3e52c53088b55218ef379
skider96 <138997737+skider96@users.noreply.github.com>
2023-12-26 21:22:17 +0200

    solving new problem

---
+++ jsExtraExcercises/JS-Fundamentals/ExerciseFunctions/3.CharactersInRange.js
@@ -0,0 +1,17 @@
+// Write a function that receives two characters and 
+// prints on a single line all the characters in between
+// them according to the ASCII code. Keep in mind that 
+//the second character code might be before the first
+// one inside the ASCII table.
+
+function charsInRange(firstChar, secondChar) {
+int
+    let firstNumber = String.charCodeAt(firstChar);
+    
+    for (let i= firstChar ; i <= secondChar ; i++) {
+        
+
+    }
+}
+charsInRange()
+
