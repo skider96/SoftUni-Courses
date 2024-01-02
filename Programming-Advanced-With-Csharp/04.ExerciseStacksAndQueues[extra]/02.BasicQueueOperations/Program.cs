e0a5fc12d4e4a2c1eeff3aa14d3be90b47292fb2
skider96 <danailqkiq@gmail.com>
2024-01-02 20:29:24 +0200

    started new course and new problems solved

---
+++ Programming-Advanced-With-Csharp/04.ExerciseStacksAndQueues[extra]/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _02.BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new();
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            int nToPush = int.Parse(input[0]);
+            int nToPop = int.Parse(input[1]);
+            int nToLook = int.Parse(input[2]);
+
+
+            int[] numbersFromConsole = new int[nToPush];
+
+            string[] secondInput = Console.ReadLine().Split().ToArray();
+            for (int i = 0; i < nToPush; i++)
+            {
+                stack.Push(int.Parse(secondInput[i]));
+            }
+
+            for (int i = 0; i < nToPop; i++)
+            {
+                stack.Pop();
+            }
+
+            bool found = stack.Contains(nToLook);
+            if (stack.Any())
+            {
+                if (found)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+
+            }
+            else Console.WriteLine(0);
+        }
+    }
+}
+// not solved
