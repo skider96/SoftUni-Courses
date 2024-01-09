3901793182b66dc0871f1310b3d787c4c28f4acb
skider96 <danailqkiq@gmail.com>
2024-01-09 23:10:56 +0200

    problem solved

--- Programming-Advanced-With-Csharp/06.Exercise-MultidimensionalArrays/01.DiagonalDifference/Program.cs
+++ Programming-Advanced-With-Csharp/06.Exercise-MultidimensionalArrays/01.DiagonalDifference/Program.cs
@@ -1,13 +1,47 @@
-﻿namespace _01.DiagonalDifference
+﻿using System.Data;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _01.DiagonalDifference
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
-            //TODO
+            int size = int.Parse(Console.ReadLine());
 
+            int[,] matrix = new int[size, size];
 
+            for (int rows = 0; rows < matrix.GetLength(1); rows++)
+            {
+                int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int cols = 0; cols < matrix.GetLength(0); cols++)
+                {
+                    matrix[rows, cols] = inputNumbers[cols];
+                }
+            }
+
+            int sumFirstDiagonal = 0;
+            int counter = 0;
+            for (int rows = 0; rows < matrix.GetLength(1); rows++)
+            {
+                sumFirstDiagonal += matrix[rows, counter];
+
+                counter++;
+            }
+
+            counter = size - 1;
+            int sumSecondDiagonal = 0;
+            for (int rows = 0; rows < matrix.GetLength(1); rows++)
+            {
+                sumSecondDiagonal += matrix[rows, counter];
+
+                counter--;
+            }
+
+            int sum = Math.Abs(sumFirstDiagonal - sumSecondDiagonal);
+            Console.WriteLine(sum);
         }
     }
 }
