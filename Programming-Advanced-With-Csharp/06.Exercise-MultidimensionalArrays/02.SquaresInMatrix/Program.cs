afa6254c0134eec1e362c025b3b053240c72c658
skider96 <danailqkiq@gmail.com>
2024-01-10 21:19:48 +0200

    trying to solve

--- Programming-Advanced-With-Csharp/06.Exercise-MultidimensionalArrays/02.SquaresInMatrix/Program.cs
+++ Programming-Advanced-With-Csharp/06.Exercise-MultidimensionalArrays/02.SquaresInMatrix/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _02.SquaresInMatrix
+﻿using System.Linq;
+
+namespace _02.SquaresInMatrix
 {
     internal class Program
     {
@@ -10,36 +12,53 @@
 
             char[][] matrix = new char[sizeRows][];
 
-            for (int row = 0; row < matrix.GetLength(1); row++)
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 char[] inputChars = Console.ReadLine().Split().Select(char.Parse).ToArray();
 
+                matrix[row] = new char[sizeCol];
                 for (int col = 0; col < sizeCol; col++)
                 {
                     matrix[row][col] = inputChars[col];
                 }
             }
-            
 
             char firstCapture = Char.MinValue;
             char secondCapture = Char.MinValue;
             int counter = 0;
-            for (int row = 0; row < matrix.GetLength(1); row++)
+            int match = 0;
+
+            Dictionary<int, List<int>> matches = new Dictionary<int, List<int>>();
+            List<int> positions = new List<int>();
+            for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                for (int col = 0; col < matrix.GetLength(0) -1; col++)
+               
+                for (int col = 0; col < sizeCol - 1; col++)
                 {
                     firstCapture = matrix[row][col];
-                    secondCapture = matrix[row][col+1];
+                    secondCapture = matrix[row][col + 1];
                     if (firstCapture == secondCapture)
                     {
-                        counter++;
+                        positions.Add(col);
+                        positions.Add(col + 1);
+                        if (!matches.ContainsKey(row))
+                        {
+                            matches.Add(row, positions);
+                        }
+                        //else
+                        //{
+                        //    matches[row].Add(col);
+                        //    matches[row].Add(col + 1);
+                        //}
                     }
+
                 }
 
 
             }
 
-            //TODO
+
         }
     }
 }
