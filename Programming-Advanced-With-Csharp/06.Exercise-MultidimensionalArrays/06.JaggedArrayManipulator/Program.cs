6dec735e360d0a54282ce53c7bfeb20049b25da9
skider96 <138997737+skider96@users.noreply.github.com>
2024-01-20 12:43:59 +0200

    solving

--- Programming-Advanced-With-Csharp/06.Exercise-MultidimensionalArrays/06.JaggedArrayManipulator/Program.cs
+++ Programming-Advanced-With-Csharp/06.Exercise-MultidimensionalArrays/06.JaggedArrayManipulator/Program.cs
@@ -5,9 +5,38 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello, world!");
+          int rows = int.Parse(Console.Readline());
 
-            //TODO
+           char[][] matrix = new char[sizeRows][];
+for(int row = 0; row < rows; row++; ) {
+ int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int colSize = input[].Length;
+
+ matrix[row] = new char[colSize];
+
+ for (int col = 0; col < colSize; col++) {
+matrix[row][col] = matrix[input[col]]
+ }
+}
+
+// analyzing the row and solving the assignment
+int previousLength = 0;
+int currentLength = 0;
+for ( int i = 0; i < n; i++;) {
+    currentLength = matrix[row][col].Length;
+
+    for (int k = 0; k < f; k++;){
+          currentLength = matrix[row][col].Length;
+
+
+if (matrix[row].Length - 1 >= 0)
+    previousLength = matrix[row] - 1
+    }
+
+}
+
+            //TO BE SOLVED
         }
     }
 }
