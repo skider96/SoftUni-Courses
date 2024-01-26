49d433d300ea23affe3ae0c73b4834e8f9dca7e7
skider96 <danailqkiq@gmail.com>
2024-01-26 19:10:34 +0200

    new problems solved

---
+++ Programming-Advanced-With-Csharp/08.Exercise-SetsAndDictionariesAdvanced/01.UniqueUsernames/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueUsernames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                uniqueUsernames.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join("\n", uniqueUsernames));
+            //TO BE CHECKED
+        }
+    }
+}
---
+++ Programming-Advanced-With-Csharp/08.Exercise-SetsAndDictionariesAdvanced/02.SetsOfElements/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] twoNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < twoNumbers[0]; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < twoNumbers[1]; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+           HashSet<int> repeatedNums = new HashSet<int>();
+            if (firstSet.Count >= secondSet.Count)
+            {
+                firstSet.IntersectWith(secondSet);
+                Print();
+            }
+            else
+            {
+                secondSet.IntersectWith(firstSet);
+                Print();
+            }
+
+            void Print()
+            {
+                Console.WriteLine(string.Join(" ", firstSet));
+            }
+
+
+            // TO BE CHECKED
+        }
+    }
+}
