941d2b21e42570fa8dbf333e9fa4fe8ccf03c4da
skider96 <danailqkiq@gmail.com>
2024-02-04 13:17:23 +0200

    solved problems

---
+++ Programming-Advanced-With-Csharp/03.StacksAndQueues/01.ReverseAString/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>(input);
+
+            while (stack.Any())
+            {
+                char popedChar = stack.Pop();
+
+                Console.Write(popedChar);
+            }
+        }
+    }
+}
---
+++ Programming-Advanced-With-Csharp/03.StacksAndQueues/02.StackSum/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new(inputNumbers);
+
+            string command;
+            while ((command = Console.ReadLine()).ToLower() != "end")
+            {
+                string[] tokens = command.Split();
+
+                if (tokens.Length == 3 && tokens[0].ToLower() == "add")
+                {
+                    stack.Push(int.Parse(tokens[1]));
+                    stack.Push(int.Parse(tokens[2]));
+                }
+                else if (tokens.Length == 2 && tokens[0].ToLower() == "remove")
+                {
+                    int numberToRemove = int.Parse(tokens[1]);
+                    if (numberToRemove > stack.Count)
+                    {
+                        continue;
+                    }
+
+                    for (int i = 0; i < numberToRemove; i++)
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+            var sum = stack.Sum();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
