de9629e6e7c20d54aa72a962cb83efaa787ae22c
skider96 <danailqkiq@gmail.com>
2024-02-05 18:35:57 +0200

    solved 1 problem  at 75% in Stacks and Queues - Lab

--- Programming-Advanced-With-Csharp/03.StacksAndQueues-Lab/08.TrafficJam/Program.cs
+++ Programming-Advanced-With-Csharp/03.StacksAndQueues-Lab/08.TrafficJam/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace _08.TrafficJam
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _08.TrafficJam
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //TODO
+            int carsThatCanPass = int.Parse(Console.ReadLine());
+
+            Queue<string> cars = new();
+
+            int carsPassed = 0;
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < carsThatCanPass; i++)
+                    {
+                        while (cars.Any())
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            carsPassed++;
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+            }
+
+            Console.WriteLine($"{carsPassed} cars passed the crossroads.");
         }
     }
 }
+// Solved but Judge gave me 75%...
