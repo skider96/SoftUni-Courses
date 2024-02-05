4fbe408e7b37ee1a1f0bee5994f9ab3d6c114362
skider96 <danailqkiq@gmail.com>
2024-02-05 14:27:56 +0200

    solved 1 problem in Stacks and Queues - Lab

--- Programming-Advanced-With-Csharp/03.StacksAndQueues-Lab/05.PrintEvenNumbers/Program.cs
+++ Programming-Advanced-With-Csharp/03.StacksAndQueues-Lab/05.PrintEvenNumbers/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            //TODO
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(input);
+
+            Console.Write(string.Join(", ", queue.Where(n => n % 2 == 0).ToList()));
         }
     }
 }
