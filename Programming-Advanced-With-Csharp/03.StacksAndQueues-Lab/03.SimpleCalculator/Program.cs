87921d74cbecc62adcba7146dd351304b3c78a79
skider96 <danailqkiq@gmail.com>
2024-02-05 13:40:26 +0200

    solved 2 problems in Stacks and Queues - Lab

--- Programming-Advanced-With-Csharp/03.StacksAndQueues-Lab/03.SimpleCalculator/Program.cs
+++ Programming-Advanced-With-Csharp/03.StacksAndQueues-Lab/03.SimpleCalculator/Program.cs
@@ -4,8 +4,45 @@
     {
         static void Main(string[] args)
         {
-            //TODO
-            
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            input = input.Reverse().ToArray();
+
+            Stack<string> stack = new(input);
+            Stack<int> resultStack = new();
+
+            int firstNubmer = 0;
+            int result = 0;
+            while (stack.Any())
+            {
+                if (stack.Count == 2 || stack.Peek() == "-" || stack.Peek() == "+")
+                {
+                    firstNubmer = result;
+                }
+                else
+                {
+                    firstNubmer = int.Parse(stack.Pop());
+
+                }
+                char operation = char.Parse(stack.Pop());
+                int secondNumber = int.Parse(stack.Pop());
+
+                if (operation == '+')
+                {
+                    result = firstNubmer + secondNumber;
+                    resultStack.Push(result);
+                }
+                else if (operation == '-')
+                {
+                    result = firstNubmer - secondNumber;
+                    resultStack.Push(result);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
--- Programming-Advanced-With-Csharp/03.StacksAndQueues-Lab/04.MatchingBrackets/Program.cs
+++ Programming-Advanced-With-Csharp/03.StacksAndQueues-Lab/04.MatchingBrackets/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace _04.MatchingBrackets
+﻿
+namespace _04.MatchingBrackets
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //TODO
+            string input = Console.ReadLine();
+            char[] chars = input.ToCharArray();
+
+            Stack<int> indexes = new Stack<int>();
+
+            List<string> results = new List<string>();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    indexes.Push(i);
+                }
+                else if (chars[i] == ')')
+                {
+                    var startIndex = indexes.Pop();
+                    var endIndex = i;
+                    string result = new (chars.Skip(startIndex).Take(endIndex - startIndex + 1).ToArray());
+
+                    results.Add(result);
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, results));
         }
     }
 }
