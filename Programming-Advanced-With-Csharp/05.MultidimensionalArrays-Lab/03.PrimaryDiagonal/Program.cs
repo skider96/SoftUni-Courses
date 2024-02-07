bccce6d5d0043b448fe4ec4e39182859e6576eab
skider96 <danailqkiq@gmail.com>
2024-02-07 19:16:52 +0200

    solved 1 problem in 05.MultidimensionalArrays-Lab

--- Programming-Advanced-With-Csharp/05.MultidimensionalArrays-Lab/03.PrimaryDiagonal/Program.cs
+++ Programming-Advanced-With-Csharp/05.MultidimensionalArrays-Lab/03.PrimaryDiagonal/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[3, 3];
+
+            int increment = 0;
+            int sumOfPrimaryDiagonal = 0;
+            for (int row = 0; row < size; row++)
+            {
+                int[]? numbersInput = Console.ReadLine()?.Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = numbersInput[col];
+                }
+
+                sumOfPrimaryDiagonal += numbersInput[increment];
+                increment++;
+            }
+
+            Console.WriteLine(sumOfPrimaryDiagonal);
         }
     }
 }
--- Programming-Advanced-With-Csharp/05.MultidimensionalArrays-Lab/04.SymbolInMatrix/Program.cs
+++ Programming-Advanced-With-Csharp/05.MultidimensionalArrays-Lab/04.SymbolInMatrix/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace _04.SymbolInMatrix
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+namespace _04.SymbolInMatrix
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a program that reads N, a number representing rows and cols of a matrix. On the next N lines, you will receive rows of the matrix.Each row consists of ASCII characters. After that, you will receive a symbol. Find the first occurrence of that symbol in the matrix and print its position in the format: "({row}, {col})".If there is no such symbol, print an error message "{symbol} does not occur in the matrix".
+
+
+
+
+
+            //TODO
         }
     }
 }
