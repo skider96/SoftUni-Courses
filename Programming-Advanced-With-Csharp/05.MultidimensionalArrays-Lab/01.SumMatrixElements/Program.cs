64394ae8ac394c0a7c436d829868d466e48f414f
skider96 <danailqkiq@gmail.com>
2024-02-07 17:42:24 +0200

    solved 2 problems in 05.MultidimensionalArrays-Lab

--- Programming-Advanced-With-Csharp/05.MultidimensionalArrays-Lab/01.SumMatrixElements/Program.cs
+++ Programming-Advanced-With-Csharp/05.MultidimensionalArrays-Lab/01.SumMatrixElements/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] rowsCols = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[rowsCols[0], rowsCols[1]];
+            int sumAllElements = 0;
+            for (int row = 0; row < rowsCols[0]; row++)
+            {
+
+                int[] inputData = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < rowsCols[1]; col++)
+                {
+                    matrix[row, col] = inputData[col];
+                    sumAllElements += inputData[col];
+                }
+            }
+
+            Console.WriteLine($"{rowsCols[0]}\n{rowsCols[1]}\n{sumAllElements}");
         }
     }
 }
--- Programming-Advanced-With-Csharp/05.MultidimensionalArrays-Lab/02.SumMatrixColumns/Program.cs
+++ Programming-Advanced-With-Csharp/05.MultidimensionalArrays-Lab/02.SumMatrixColumns/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace _02.SumMatrixColumns
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace _02.SumMatrixColumns
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] rowsCols = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+            .ToArray();
+            int[,] matrix = new int[rowsCols[0], rowsCols[1]];
+
+            int[] sumNumbersInCols = new int[rowsCols[1]];
+
+            for (int row = 0; row < rowsCols[0]; row++)
+            {
+                int[] inputData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < rowsCols[1]; col++)
+                {
+                    matrix[row, col] = inputData[col];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int numbersInCols = 0;
+
+                for (var row = 0; row < matrix.GetLength(0); row++)
+                {
+                    numbersInCols += matrix[row, col];
+                }
+
+                sumNumbersInCols[col] = numbersInCols;
+            }
+
+            foreach (var numbers in sumNumbersInCols)
+            {
+                Console.WriteLine(numbers);
+            }
         }
     }
 }
