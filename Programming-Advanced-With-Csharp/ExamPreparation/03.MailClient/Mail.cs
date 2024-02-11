b0a19d7c64c58d29970925064ff2ecc6ba94a18e
skider96 <danailqkiq@gmail.com>
2024-02-11 13:39:06 +0200

    solved 1 problem in ExamPreparation in C# Advanced

--- Programming-Advanced-With-Csharp/ExamPreparation/03.MailClient/Mail.cs
+++ Programming-Advanced-With-Csharp/ExamPreparation/03.MailClient/Mail.cs
@@ -2,5 +2,19 @@
 {
     public class Mail
     {
+        public Mail(string sender, string receiver, string body)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            Body = body;
+        }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Body { get; set; }
+
+        public override string ToString()
+        {
+            return $"From: {Sender} / To: {Receiver}\nMessage: {Body}";
+        }
     }
 }
--- Programming-Advanced-With-Csharp/ExamPreparation/03.MailClient/MailBox.cs
+++ Programming-Advanced-With-Csharp/ExamPreparation/03.MailClient/MailBox.cs
@@ -1,6 +1,61 @@
-﻿namespace MailClient
+﻿using System.Text;
+
+namespace MailClient
 {
     public class MailBox
     {
+        public int Capacity { get; set; }
+
+        public List<Mail> Inbox { get; set; }
+
+        public List<Mail> Archive { get; set; }
+
+        public MailBox(int capacity)
+        {
+            Capacity = capacity;
+            Inbox = new List<Mail>();
+            Archive = new List<Mail>();
+        }
+
+        public void IncomingMail(Mail mail)
+        {
+            if (Capacity > Inbox.Count)
+            {
+                Inbox.Add(mail);
+            }
+        }
+
+        public bool DeleteMail(string sender)
+        {
+            var toBeRemoved = Inbox.FirstOrDefault(s => s.Sender == sender);
+            return Inbox.Remove(toBeRemoved);
+        }
+
+        public int ArchiveInboxMessages()
+        {
+            Archive.AddRange(Inbox);
+            var mailMoved = Inbox.Count;
+            Inbox.Clear();
+            return mailMoved;
+        }
+
+        public string GetLongestMessage()
+        {
+            var longestMail = Inbox.MaxBy(b => b.Body.Length);
+
+            return longestMail.ToString();
+        }
+
+        public string InboxView()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Inbox:");
+            foreach (var mail in Inbox)
+            {
+                sb.AppendLine(mail.ToString());
+            }
+
+            return sb.ToString();
+        }
     }
 }
