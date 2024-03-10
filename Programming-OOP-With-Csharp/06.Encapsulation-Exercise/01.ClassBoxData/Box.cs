f97d77eee14bd663bb9fb07996c2980ff5006be7
skider96 <danailqkiq@gmail.com>
2024-03-10 20:10:29 +0200

    solved 1 problem in 06.Encapsulation-Exercise

---
+++ Programming-OOP-With-Csharp/06.Encapsulation-Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Double = System.Double;
+
+namespace _01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        public double Length
+        {
+            get => length;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get => height;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * Length * Width + LateralSurfaceArea();
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * Length * Height + 2 * Width * Height;
+        }
+
+        public double Volume()
+        {
+            return Length * Width * Height;
+        }
+
+        public override string ToString()
+        => $"Surface Area - {SurfaceArea():f2}\nLateral Surface Area - {LateralSurfaceArea():f2}\nVolume - {Volume():f2}";
+    }
+}
---
+++ Programming-OOP-With-Csharp/06.Encapsulation-Exercise/01.ClassBoxData/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.ClassBoxData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new(length, width, height);
+                Console.WriteLine(box.ToString());
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
